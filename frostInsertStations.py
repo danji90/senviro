e5b85cac65aa0c51cc1ac0e8ae8745472e51ef6d
danji90 <danji_ma90@hotmail.com>
2018-11-19 22:09:01 +0100

    added frostInsertStations script, updated broker.py

--- frostInsertStations.py
+++ frostInsertStations.py
@@ -5,7 +5,6 @@ import sys
 # Data
 sensorData = [{"description":"Photon WeatherShield Atmospheric Sensors","name":"PhotonWeatherShield", "encodingType":"text/plan","metadata":"http://www.senviro.uji.es"},{"description":"Measures wind speed and direction","name":"Anemometer", "encodingType":"text/plan","metadata":"http://www.senviro.uji.es"},{"description":"Measures soil moisture","name":"SoilMoistureSensor", "encodingType":"text/plan","metadata":"http://www.senviro.uji.es"},{"description":"Measures soil temperature","name":"DS18B20", "encodingType":"text/plan","metadata":"http://www.senviro.uji.es"}, {"description":"Measures rainfall","name":"Pluviometer", "encodingType":"text/plan","metadata":"http://www.senviro.uji.es"}, {"description":"Battery Readings","name":"Battery", "encodingType":"text/plan","metadata":"http://www.senviro.uji.es"}]
 
-
 # FROST-Server baseUrl
 baseUrl = "http://elcano.init.uji.es:8082/FROST-Server/v1.0"
 
